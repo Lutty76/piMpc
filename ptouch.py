8362e6289026a9e0d04ff567533197b12f6f4d1f
Lutty <kevin_vezier@yahoo.fr>
2018-10-05 21:55:20 +0200

    optimize code

--- ptouch.py
+++ ptouch.py
@@ -17,7 +17,6 @@ def handle_touch(event):
     button[event.name] = time.time()
     longPress[event.name] = False
 
-
 @touchphat.on_release(['Back','A','B','C','D','Enter'])
 def handle_release(event):
     global button
@@ -28,7 +27,7 @@ def handle_release(event):
 
 def handle_button(button,longPress):
     switcher={
-            0:lamda :"Not a button",
+            0:lambda: "Not a button",
             1:handle_enter,
             2:handle_a,
             3:handle_b,
@@ -40,7 +39,7 @@ def handle_button(button,longPress):
 
 def handle_enter(longPress):
     if longPress:
-            os.system('mpc toggle')
+        os.system('mpc toggle')
     else:
         os.system('mpc prev')
 
@@ -59,11 +58,8 @@ def handle_a(longPress):
         os.system('mpc findadd genre \'Hard Rock\'')
         os.system('mpc findadd genre Classical')
         os.system('mpc play')
-        touchphat.led_off("A")
+        touchphat.all_off()
         touchphat.led_on("A")
-        touchphat.led_off("B")
-        touchphat.led_off("C")
-        touchphat.led_off("D")
 
 def handle_b(longPress):
     if longPress:
@@ -73,11 +69,8 @@ def handle_b(longPress):
         os.system('mpc findadd genre Rock')
         os.system('mpc findadd genre Blues')
         os.system('mpc play')
-        touchphat.led_off("B")
-        touchphat.led_off("A")
+        touchphat.all_off()
         touchphat.led_on("B")
-        touchphat.led_off("C")
-        touchphat.led_off("D")
 
 def handle_c(longPress):
     if longPress:
@@ -90,11 +83,8 @@ def handle_c(longPress):
         os.system('mpc findadd genre Rock')
         os.system('mpc findadd genre Blues')
         os.system('mpc play')
-        touchphat.led_off("C")
-        touchphat.led_off("A")
-        touchphat.led_off("B")
+        touchphat.all_off()
         touchphat.led_on("C")
-        touchphat.led_off("D")
 
 def handle_d(longPress):
     if longPress:
@@ -103,37 +93,23 @@ def handle_d(longPress):
         os.system('mpc clear')   
         os.system('mpc ls |mpc add')   
         os.system('mpc play')
-        touchphat.led_off("D")
-        touchphat.led_off("A")
-        touchphat.led_off("B")
-        touchphat.led_off("C")
+        touchphat.all_off()
         touchphat.led_on("D")
-
-
 
 def center():
     i=6
     while i>0:
+        touchphat.all_off()
         touchphat.led_on(1)
         touchphat.led_on(6)
-        touchphat.led_off(4)
-        touchphat.led_off(2)
-        touchphat.led_off(3)
-        touchphat.led_off(5)
         time.sleep(0.12)
+        touchphat.all_off()
         touchphat.led_on(2)
         touchphat.led_on(5)
-        touchphat.led_off(4)
-        touchphat.led_off(1)
-        touchphat.led_off(3)
-        touchphat.led_off(6)
         time.sleep(0.12)
-        touchphat.led_on(4)
+        touchphat.all_off()
         touchphat.led_on(3)
-        touchphat.led_off(2)
-        touchphat.led_off(1)
-        touchphat.led_off(6)
-        touchphat.led_off(5)
+        touchphat.led_on(4)
         time.sleep(0.12)
         i-=1
     global lost
@@ -142,20 +118,16 @@ def center():
 def christmas():
     i=6
     while i>0:
+        touchphat.all_off()
         touchphat.led_on(1)
-        touchphat.led_off(2)
         touchphat.led_on(3)
-        touchphat.led_off(4)
         touchphat.led_on(5)
-        touchphat.led_off(6)
-        time.sleep(0.12)
-        touchphat.led_off(1)
+        time.sleep(0.24)
+        touchphat.all_off()
         touchphat.led_on(2)
-        touchphat.led_off(3)
         touchphat.led_on(4)
-        touchphat.led_off(5)
         touchphat.led_on(6)
-        time.sleep(0.12)
+        time.sleep(0.24)
         i-=1
     global lost
     lost = True
@@ -182,37 +154,21 @@ def k2000():
     global lost
     lost = True
     
-
-
 try:
     while True:
         if lost:
             lost = False
             p = subprocess.Popen(['mpc', 'playlist', '-f', '%genre%'],stdout=subprocess.PIPE)
-            output = subprocess.check_output(('sort','-u'), stdin=p.stdout)
-            listGenre = output.split('\n')
-            touchphat.led_off('Enter')
-            touchphat.led_off('Back')
+            listGenre = subprocess.check_output(('sort','-u'), stdin=p.stdout).split('\n')
+            touchphat.all_off()
             if 'Fusion' in listGenre :
                 touchphat.led_on('D')
-                touchphat.led_off('A')
-                touchphat.led_off('B')
-                touchphat.led_off('C')
             elif 'Rock'  in listGenre and 'Classical' in listGenre :
                 touchphat.led_on('C')
-                touchphat.led_off('A')
-                touchphat.led_off('B')
-                touchphat.led_off('D')
             elif 'Rock'  in listGenre :
                 touchphat.led_on('B')
-                touchphat.led_off('A')
-                touchphat.led_off('C')
-                touchphat.led_off('D')
             else:
                 touchphat.led_on('A')
-                touchphat.led_off('C')
-                touchphat.led_off('B')
-                touchphat.led_off('D')
            
         for pad in ['Back','A','B','C','D','Enter']:
             if button[pad] != 0:
